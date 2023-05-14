ddf12222704fc3564c7f9ce95341f3850bd3e40c
3r1cZ <ericzhoubusiness0@gmail.com>
2023-05-14 13:46:24 -0400

    update styling of button when clicked
    in index.html

--- functions.js
+++ functions.js
@@ -3,16 +3,19 @@ function smoothTransition(){
     const container = document.querySelector('.expansion-container');
     const text = document.querySelector(".expanding-text");
     const mainPage = document.querySelector(".page-intro");
+    const button = document.querySelector(".expanding-button");
     if(open == false){
         container.style.display = "block";
         text.style.display = "block";
         mainPage.style.height = "500px";
+        button.style.background = "antiquewhite";
         open = true;
     }
     else{
         container.style.display = "none";
         text.style.display = "none";
         mainPage.style.height = "400px";
+        button.style.background = "cornsilk";
         open = false;
     }
 }
